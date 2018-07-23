83fbe78459a597db7f10c76e3ea468643c4e0682
Consciousnesszz <consciousnesszz1@gmail.com>
2018-07-23 18:07:29 +0800

    初步完成 client

---
+++ client/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-dom';
+// import { Router, match, browserHistory } from 'react-router';
+// import { Provider } from 'react-redux';
+
+// import router from './router';
+
+// const store = configureStore(window.REDUX_STATE);
+
+// match({ history: browserHistory, router }, (err, redirectLocation, renderProps) => {
+//   render(
+//     <Provider store={store}>
+//       <Router {...renderProps} />
+//     </Provider>,
+//     document.getElementById('root')
+//   );
+// });
+render(
+  <div>
+    root
+  </div>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './client/index.js',
+    vendor: ['react', 'react-dom', 'react-router', 'redux', 'react-redux'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  resolve: {
+    extensions: ['.js', '.less'],
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // js
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015'],
+          },
+        }],
+        exclude: /node_modules/,
+      }, {
+        test: /\.less$/, // less
+        use: [
+          {
+            loader: 'style-loader',
+          }, {
+            loader: 'css-loader',
+            options: {
+              minimize: false,
+            },
+          }, {
+            loader: 'less-loader',
+          },
+        ],
+        exclude: /node_modules/,
+      }, {
+        test: /\.css$/, // css
+        use: [
+          {
+            loader: 'style-loader',
+          }, {
+            loader: 'css-loader',
+            options: {
+              minimize: false,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      }, {
+        test: /\.(png|jpg|gif)$/, // 图片
+        use: [
+          {
+            loader: 'url-loader?limit=20000&name=images/[hash:8].[name].[ext]',
+          },
+        ],
+        exclude: /node_modules/,
+      }, {
+        test: /\.(woff|svg|eot|ttf)$/, // 字体
+        use: [
+          {
+            loader: 'url-loader?name=font/[hash:8].[name].[ext]',
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './index.temp.html' }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'vendor',
+    //   filename: './vendor.js',
+    // }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false,
+    //   },
+    // })
+  ],
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      filename: './vendor.js',
+    },
+  },
+};
