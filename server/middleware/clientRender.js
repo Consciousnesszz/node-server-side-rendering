d46d7b4577eccf86c2e3161e8ea571567a86661d
zjh <zhangjianhao@haixue.com>
2018-09-27 13:09:56 +0800

    add else

--- server/middleware/clientRender.js
+++ server/middleware/clientRender.js
@@ -28,22 +28,22 @@ async function clientRender(ctx, next) {
     const htmlData = fs.readFileSync(path.resolve(tempPath), 'utf-8');
     if (!htmlData) {
       ctx.body = 404;
+    } else {
+      const context = {};
+      // 使用 服务端渲染 函数
+      const html = renderToString(
+        <Provider store={store}>
+          <StaticRouter
+            location={ctx.url}
+            context={context}
+          >
+            <App />
+          </StaticRouter>
+        </Provider>
+      );
+
+      ctx.body = htmlData.replace('{{replace}}', html);
     }
-
-    const context = {};
-    // 使用 服务端渲染 函数
-    const html = renderToString(
-      <Provider store={store}>
-        <StaticRouter
-          location={ctx.url}
-          context={context}
-        >
-          <App />
-        </StaticRouter>
-      </Provider>
-    );
-
-    ctx.body = htmlData.replace('{{replace}}', html);
   } else {
     // 不调用 next 会找不到 js
     await next();
